a8e97d25cfb1eacdb859313a84925356fc1c9694
junio10 <junio.js977@gmail.com>
2022-11-17 19:36:40 -0300

    Testando.

--- knn.py
+++ knn.py
@@ -2,8 +2,10 @@ import pandas as pd
 import preprocessing as pr
 from sklearn.neighbors import KNeighborsClassifier
 
+url = 'https://raw.githubusercontent.com/junio10/Projeto-KNN-ANEMIA/main/anemia.csv?token=GHSAT0AAAAAAB2PNRK2VLCYNQ37HQ63EZEOY3WXE5A'
+base_arquivo = pd.read_csv(url)
+
 modelo = KNeighborsClassifier(n_neighbors = 3)
 modelo.fit(normalized_attributes, classes)
-pd.read_csv()
 
 
